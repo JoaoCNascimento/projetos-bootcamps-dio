3edef5cec4b3bf8c299345b79ac11c953898818f
João Nascimento <joao.nascimento0576@gmail.com>
2021-09-03 14:20:55 -0300

    Primeiro commit

---
+++ Santander Fullstack Developer Bootcamp/frontend & backend/saladereuniao/src/main/java/com/digital/crud/reuniao/saladereuniao/model/Room.java
@@ -0,0 +1,43 @@
+package com.digital.crud.reuniao.saladereuniao.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "meeting_room")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "start_hour", nullable = false)
+    private String startHour;
+
+    @Column(name = "end_hour", nullable = false)
+    private String endHour;
+
+    @Override
+    public String toString() {
+        return "Room [ id=" + id + ",nome=" + name + ",date=" + date + ",startHour=" + startHour + ",endHour=" + endHour
+                + " ]";
+    }
+
+}
---
+++ Santander Fullstack Developer Bootcamp/frontend & backend/saladereuniao/src/main/java/com/digital/crud/reuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.digital.crud.reuniao.saladereuniao.repository;
+
+import com.digital.crud.reuniao.saladereuniao.model.Room;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+}
